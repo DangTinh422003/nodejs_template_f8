5f7e863e2753d1ba287994a60c11a9a5ca46b139
Cao Dang Tinh <caodangtinh04022003@gmail.com>
2023-10-10 02:05:01 +0700

    add MVC model

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { engine } = require("express-handlebars");
 const path = require("path");
 const app = express();
 const port = 3000;
+const route = require("./routes/index.route");
 
 // ** HTTP logger **
 app.use(morgan("tiny"));
@@ -17,9 +18,7 @@ app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
 
 // ** router
-app.get("/", (req, res) => {
-  res.render("pages/home");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ src/routes/index.route.js
@@ -0,0 +1,11 @@
+const newsRouter = require("./news.route");
+
+const route = (app) => {
+  app.use("/news", newsRouter);
+  
+  app.get("/", (req, res) => {
+    res.render("pages/home");
+  });
+};
+
+module.exports = route;
